ef7f31d5983775d31b930c63cf3e2b4513512d5f
bluwy <bjornlu.dev@gmail.com>
2021-01-20 20:32:38 +0800

    Remove unused code

--- src/views/Dashboard.svelte
+++ src/views/Dashboard.svelte
@@ -6,64 +6,33 @@
   import Logo from '../components/Logo.svelte';
   import NetworkConnect from '../components/NetworkConnect.svelte';
   import NetworkList from '../components/NetworkList.svelte';
-  import { tick } from 'svelte';
 
   export let state;
   export let api;
 
-  let safeToShow = false;
   let selectedNetwork;
-  // let card;
   let cardWidth;
-  let cardHeight;
-  let cardHeightValue;
   let cardFlexHeight = tweened(0, { easing: quadOut, duration: 200 });
 
   $: networks = state.wifiNetworks || [];
 
-  setTimeout(() => (safeToShow = true), 200);
-
   function connectNetwork(apssid, password) {
     api('wifi').call('setup', { apssid, password });
+    // Go back to network list
     selectedNetwork = undefined;
-  }
-
-  async function setCardFlexHeight(v) {
-    cardHeightValue = '';
-
-    $cardFlexHeight = v;
-
-    // wait animate
-    await tick();
-
-    cardHeightValue = cardHeight + 'px';
   }
 </script>
 
 <div class="wrapper">
   <Logo />
   <p>Connect to a Wifi network below</p>
-  <div
-    bind:offsetWidth={cardWidth}
-    class="card"
-    style="flex-basis: {$cardFlexHeight}px; visibility: {safeToShow ? 'visible' : 'hidden'};"
-  >
+  <div bind:offsetWidth={cardWidth} class="card" style="flex-basis: {$cardFlexHeight}px;">
     {#if selectedNetwork == null}
-      <div
-        use:resize
-        class="card-content"
-        transition:fly|local={{ x: -cardWidth }}
-        on:elResize={e => setCardFlexHeight(e.detail.target.scrollHeight)}
-      >
+      <div use:resize class="card-content" transition:fly|local={{ x: -cardWidth }} on:elResize={e => ($cardFlexHeight = e.detail.target.scrollHeight)}>
         <NetworkList {networks} on:selectNetwork={e => (selectedNetwork = e.detail)} />
       </div>
     {:else}
-      <div
-        use:resize
-        class="card-content"
-        transition:fly|local={{ x: cardWidth }}
-        on:elResize={e => setCardFlexHeight(e.detail.target.scrollHeight)}
-      >
+      <div use:resize class="card-content" transition:fly|local={{ x: cardWidth }} on:elResize={e => ($cardFlexHeight = e.detail.target.scrollHeight)}>
         <NetworkConnect network={selectedNetwork} on:cancel={() => (selectedNetwork = undefined)} on:connect={e => connectNetwork(selectedNetwork, e.detail)} />
       </div>
     {/if}
