57103c1fa5d0ffe8c870caeedd6bf26d02d4c8b2
bluwy <bjornlu.dev@gmail.com>
2021-01-24 22:26:47 +0800

    Use searchable list component

--- src/components/NetworkList.svelte
+++ src/components/NetworkList.svelte
@@ -1,110 +1,33 @@
 <script>
-  import { createEventDispatcher, onMount } from 'svelte';
-  import { List, ListItem } from 'dmt-frontend-components';
+  import { createEventDispatcher } from 'svelte';
+  import { SearchableList } from 'dmt-frontend-components';
   import ChevronRightIcon from './ChevronRightIcon.svelte';
 
   const dispatch = createEventDispatcher();
 
   export let networks = [];
-
-  let search = '';
-  let highlightNetworkIndex = -1;
-
-  $: searchLowercased = search.toLowerCase();
-
-  $: filteredNetworks = search ? networks.filter(v => v.toLowerCase().includes(searchLowercased)) : networks;
-
-  $: if (search) {
-    highlightNetworkIndex = filteredNetworks.length > 0 ? 0 : -1;
-  }
-
-  onMount(() => {
-    window.addEventListener('keydown', handleKeyDown);
-    return () => window.removeEventListener('keydown', handleKeyDown);
-  });
-
-  function handleKeyDown(e) {
-    if (e.key === 'ArrowUp') {
-      highlightNetworkIndex--;
-      if (highlightNetworkIndex < 0) {
-        highlightNetworkIndex = filteredNetworks.length - 1;
-      }
-    } else if (e.key === 'ArrowDown') {
-      highlightNetworkIndex++;
-      if (highlightNetworkIndex >= filteredNetworks.length) {
-        highlightNetworkIndex = 0;
-      }
-    } else if (e.key === 'Enter') {
-      dispatch('selectNetwork', filteredNetworks[highlightNetworkIndex]);
-    }
-  }
-
-  function handleSearchKeyDown(e) {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-    }
-  }
 </script>
 
-<List transparent focusTop>
-  <ListItem fluid>
-    <!-- svelte-ignore a11y-autofocus -->
-    <input type="search" bind:value={search} on:keydown={handleSearchKeyDown} autofocus placeholder="Search network..." />
-  </ListItem>
-  {#each filteredNetworks as network, i}
-    <ListItem fluid>
-      <button class:highlight={highlightNetworkIndex === i} on:click={() => dispatch('selectNetwork', network)}>
-        <span>
-          {network}
-        </span>
-        <ChevronRightIcon />
-      </button>
-    </ListItem>
-  {:else}
-    <ListItem>
-      <p>No networks found</p>
-    </ListItem>
-  {/each}
-</List>
+<SearchableList
+  items={networks}
+  transparent
+  searchPlaceholder="Search networks..."
+  searchAutofocus
+  noItemsText="No networks found"
+  on:selectItem={e => dispatch('selectNetwork', e.detail)}
+>
+  <div class="item" slot="item" let:item>
+    <span>
+      {item}
+    </span>
+    <ChevronRightIcon />
+  </div>
+</SearchableList>
 
 <style>
-  input {
-    width: 100%;
-    padding: 0.8rem 1rem;
-    border-radius: 0;
-    background-color: transparent;
-    color: white;
-    font-weight: 600;
-  }
-
-  button {
+  .item {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    width: 100%;
-    padding: 0.625rem 1rem;
-    border-radius: 0;
-    outline: none;
-    border: 0;
-    color: white;
-    background-color: transparent;
-    text-align: left;
-    transition: color 0.2s, background-color 0.2s;
-  }
-
-  button:hover {
-    color: rgb(var(--dmt-cool-cyan-rgb));
-    background-color: rgba(var(--dmt-cool-cyan-rgb), 0.15);
-  }
-
-  button:focus,
-  button.highlight {
-    color: rgb(var(--dmt-cool-cyan-rgb));
-    background-color: rgba(var(--dmt-cool-cyan-rgb), 0.2);
-  }
-
-  p {
-    text-align: center;
-    opacity: 0.5;
   }
 </style>
